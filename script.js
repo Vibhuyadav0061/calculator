daa72ba21e0768e230e877876ba9db99b3a1a7e2
VIBHU YADAV <113329071+Vibhuyadav0061@users.noreply.github.com>
2023-08-13 22:40:06 +0530

    Add files via upload
    correction

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ Array.from(btn).forEach((btn) => {
             string = '';
             document.getElementById('display').value = string;
         }
-        // else if(e.target.innerHTML = 'DC'){
-        //     string = '';
-        //     document.getElementById('display').value = string;
-        // }
+        else if(e.target.innerHTML == 'CE'){
+            // string = string.toString().slice(0,-1);
+            document.getElementById('display').value = document.getElementById('display').value.toString().slice(0,-1) ;
+        }
         else{
             string = string + e.target.innerHTML;
             document.getElementById('display').value = string;
